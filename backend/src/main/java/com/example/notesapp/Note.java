f8ba40594d0f8da8cbc84d9e23054617917dce37
Colton Randall <coltonrandall.nz@gmail.com>
2025-07-01 20:12:47 +1200

    move api project to backend folder

---
+++ backend/src/main/java/com/example/notesapp/Note.java
@@ -0,0 +1,14 @@
+package com.example.notesapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Note {
+    private String title;
+    private String content;
+    private String id;
+}
